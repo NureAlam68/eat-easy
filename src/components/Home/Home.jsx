51533531c2397d4970cdff3d0515b6dc67908e7b
NureAlam68 <nurealam151068@gmail.com>
2024-10-29 23:22:25 +0600

    added footer

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,15 @@
+import { Outlet } from "react-router-dom";
 import Banner from "../Banner/Banner";
 import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
 
 const Home = () => {
     return (
         <div className="max-w-[1600px] container mx-auto">
             <Header></Header>
             <Banner></Banner>
+            <Outlet></Outlet>
+            <Footer></Footer>
             
         </div>
     );
